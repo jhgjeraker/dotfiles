6b953af488edd4b8ec11f292ab39354815c865ff
Johannes H. Gjeraker <johannes.holm.gjeraker@disruptive-technologies.com>
2023-01-12 09:23:25 +0100

    improved search grep

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -264,10 +264,17 @@ end, { desc = '[/] Fuzzily search in current buffer]' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 vim.keymap.set('n', '<leader>sb', require('telescope.builtin').builtin, { desc = '[S]earch [B]uiltins' })
+vim.keymap.set('n', '<leader>sg', function()
+    require('telescope.builtin').grep_string{
+        shorten_path=true,
+        word_match="-w",
+        only_sort_text=true,
+        search='',
+    }
+end, { desc = '[S]earch [G]rep' })
 
 -------------------------------------------------------------------------------
 --                                                             hrsh7th/nvim-cmp
