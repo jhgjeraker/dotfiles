fa28e12cf903500f46f978aef4dc8dcf02af3d26
Johannes H. Gjeraker <johannes.holm.gjeraker@disruptive-technologies.com>
2023-06-01 18:17:07 +0200

    updated neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -80,7 +80,7 @@ local plugins = {
             cmp.setup({
                 snippet = {
                     expand = function(args)
-                        require("lurasnip").lsp_expand(args.body)
+                        require("luasnip").lsp_expand(args.body)
                     end,
                 },
                 mapping = cmp.mapping.preset.insert {
@@ -121,9 +121,33 @@ local plugins = {
             }
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+            capabilities.textDocument.foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true
+            }
+
+            on_attach = function(client, bufnr)
+                local nmap = function(keys, func, desc)
+                    if desc then
+                        desc = 'LSP: ' .. desc
+                    end
+                    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+                end
+
+                -- Buffer local mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                nmap('gd', vim.lsp.buf.definition, "[g]oto [d]efinition")
+                nmap('gD', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', "[g]oto [D]efinition in new tab")
+                nmap('gr', vim.lsp.buf.references, "[g]oto [r]erences")
+                nmap('K', vim.lsp.buf.hover, "hover")
+                nmap('<leader>rn', vim.lsp.buf.rename, "rename")
+                nmap('<leader>f', function() vim.lsp.buf.format { async = true } end, "format")
+            end
+
             mason_lspconfig.setup_handlers {
                 function(server_name)
                     require('lspconfig')[server_name].setup {
+                        on_attach = on_attach,
                         capabilities = capabilities,
                         settings = servers[server_name],
                     }
@@ -204,7 +228,76 @@ local plugins = {
                 },
             }
         end,
-    }
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        cmd = "Neotree", -- lazy-load on command Neotree
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            -- remove the deprecated commands from v1.x
+            vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+            require("neo-tree").setup({
+                auto_clean_after_session_restore = true,
+                close_if_last_window = true,
+                sources = {
+                    "filesystem",
+                    "git_status",
+                },
+                source_selector = {
+                    winbar = true,
+                    content_layout = "center",
+                    sources = {
+                        { source = "filesystem", display_name = "File" },
+                        { source = "git_status", display_name = "Git" },
+                    },
+                },
+                window = {
+                    width = 30,
+                },
+                filesystem = {
+                    follow_current_file = true,
+                    hijack_netrw_behavior = "open_current",
+                    use_libuv_file_watcher = true,
+                },
+            })
+        end,
+    },
+    {
+        'mrjones2014/smart-splits.nvim',
+    },
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = {
+            "kevinhwang91/promise-async"
+        },
+        ft = languages,
+        config = function()
+            local ftMap = {
+                python = { 'indent' },
+            }
+            require('ufo').setup({
+                open_fold_hl_timeout = 0,
+                provider_selector = function(bufnr, filetype, buftype)
+                    return ftMap[filetype]
+                end
+            })
+            vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+        end,
+    },
+    {
+        "RRethy/vim-illuminate",
+        ft = languages,
+        config = function()
+            require('illuminate').configure({
+                delay = 100,
+            })
+        end,
+    },
 }
 
 local opts = {
@@ -226,18 +319,51 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
 
-vim.o.ignorecase = true                         -- case insensitive search
-vim.keymap.set('n', '<ESC>', ':nohlsearch<CR>') -- remove highlight by pressing Esc
+vim.o.ignorecase = true                                      -- case insensitive search
+vim.keymap.set('n', '<ESC>', ':nohlsearch<CR>')              -- remove highlight by pressing Esc
 
-vim.o.number = true                             -- line numbering
-vim.o.relativenumber = true                     -- relative line numbering
+vim.o.completeopt = "menu,menuone,noselect"                  -- Options for insert mode completion
 
+vim.o.number = true                                          -- line numbering
+vim.o.relativenumber = true                                  -- relative line numbering
+vim.o.ruler = true                                           -- cursor ruler
+vim.o.cursorline = true                                      -- cursor ruler
+
+vim.o.foldenable = true                                      -- enable fold for nvim-ufo
+vim.o.foldlevel = 99                                         -- set high foldlevel for nvim-ufo
+vim.o.foldlevelstart = 99                                    -- start with all code unfolded
+vim.o.foldcolumn = vim.fn.has "nvim-0.9" == 1 and "1" or nil -- show foldcolumn in nvim 0.9
+
+------------------------------------------------------------------------------
+--                                                                    Keybinds
 -- Copy and paste to system clipboard.
 vim.keymap.set('', '<Leader>y', '"+y')
 vim.keymap.set('', '<Leader>p', '"+p')
 
+-- Telescope
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+
+-- Neotree
+vim.keymap.set('n', '<leader>E', '<cmd>Neotree toggle<cr>')
+vim.keymap.set('n', '<leader>e', function()
+    if vim.bo.filetype == "neo-tree" then
+        vim.cmd.wincmd "p"
+    else
+        vim.cmd.Neotree "focus"
+    end
+end)
+
+-- smart-splits
+vim.keymap.set('n', '<C-Left>', require('smart-splits').resize_left)
+vim.keymap.set('n', '<C-Down>', require('smart-splits').resize_down)
+vim.keymap.set('n', '<C-Up>', require('smart-splits').resize_up)
+vim.keymap.set('n', '<C-Right>', require('smart-splits').resize_right)
+-- moving between splits
+vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
 
 ------------------------------------------------------------------------------
 --                                                                LSP Keybinds
@@ -251,26 +377,26 @@ vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-        local nmap = function(keys, func, desc)
-            if desc then
-                desc = 'LSP: ' .. desc
-            end
-            vim.keymap.set('n', keys, func, { buffer = ev.buf, desc = desc })
-        end
-
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        nmap('gd', vim.lsp.buf.definition, "[g]oto [d]efinition")
-        nmap('gD', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', "[g]oto [D]efinition in new tab")
-        nmap('gr', vim.lsp.buf.references, "[g]oto [r]erences")
-        nmap('K', vim.lsp.buf.hover, "hover")
-        nmap('<leader>rn', vim.lsp.buf.rename, "rename")
-        nmap('<leader>f', function() vim.lsp.buf.format { async = true } end, "format")
-    end,
-})
+--vim.api.nvim_create_autocmd('LspAttach', {
+--    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+--    callback = function(ev)
+--        -- Enable completion triggered by <c-x><c-o>
+--        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+--
+--        local nmap = function(keys, func, desc)
+--            if desc then
+--                desc = 'LSP: ' .. desc
+--            end
+--            vim.keymap.set('n', keys, func, { buffer = ev.buf, desc = desc })
+--        end
+--
+--        -- Buffer local mappings.
+--        -- See `:help vim.lsp.*` for documentation on any of the below functions
+--        nmap('gd', vim.lsp.buf.definition, "[g]oto [d]efinition")
+--        nmap('gD', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', "[g]oto [D]efinition in new tab")
+--        nmap('gr', vim.lsp.buf.references, "[g]oto [r]erences")
+--        nmap('K', vim.lsp.buf.hover, "hover")
+--        nmap('<leader>rn', vim.lsp.buf.rename, "rename")
+--        nmap('<leader>f', function() vim.lsp.buf.format { async = true } end, "format")
+--    end,
+--})
